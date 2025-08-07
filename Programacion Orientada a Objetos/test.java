5169d38ca3ff08ea672c8563690642caab65690f
AnniesTech <amarie.aguirre05@gmail.com>
2025-08-07 15:27:35 -0500

    Created the directory for the new course, oop in java

---
+++ Programacion Orientada a Objetos/test.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+/**
+ *
+ * @author Amarie
+ */
+public class test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        for(int i = 1; i <= 10; i++){
+            System.out.println(i);
+        }
+    }
+    
+}
